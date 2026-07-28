45629e2366b2c99d475cff7af79710d5aa8e0db8
yenugu-merget <sanjay.yenugu@zohomail.in>
2026-07-28 12:52:08 +0530

    Create Employee Dashboard (Version 1)
    Added the initial Employee Dashboard with basic HTML layout, CSS styling, and JavaScript to load a sample employee list and display the employee count.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-console.log("Employee Management System");
+function loadEmployees(){
+
+const employees=[
+    "Sanjay",
+    "John",
+    "David",
+    "Anita"
+];
+
+document.getElementById("count").innerHTML=employees.length;
+
+const list=document.getElementById("employeeList");
+
+list.innerHTML="";
+
+employees.forEach(function(emp){
+
+const li=document.createElement("li");
+
+li.innerHTML=emp;
+
+list.appendChild(li);
+
+});
+
+}
