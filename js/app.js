7c7df020fdf4088529fed1b4c364642b691a5bf5
yenugu-merget <sanjay.yenugu@zohomail.in>
2026-07-28 14:32:40 +0530

    employee ID update

--- js/app.js
+++ js/app.js
@@ -1,26 +1,81 @@
-function loadEmployees(){
 
-const employees=[
-    "Sanjay",
-    "John",
-    "David",
-    "Anita"
-];
+console.log("app.js loaded");
 
-document.getElementById("count").innerHTML=employees.length;
+function loadEmployees() {
 
-const list=document.getElementById("employeeList");
+    document.getElementById("count").innerHTML = employees.length;
 
-list.innerHTML="";
+    const list = document.getElementById("employeeList");
 
-employees.forEach(function(emp){
+    list.innerHTML = "";
 
-const li=document.createElement("li");
+    employees.forEach(function(emp){
 
-li.innerHTML=emp;
+        const li = document.createElement("li");
 
-list.appendChild(li);
+        li.innerHTML =
+            emp.id +
+            " | " +
+            emp.name +
+            " | " +
+            emp.department +
+            " | " +
+            emp.designation;
 
-});
+        list.appendChild(li);
+
+    });
 
 }
+
+const employees = [
+    {
+        id: "EMP101",
+        name: "Sanjay",
+        department: "QA",
+        designation: "QA Manager",
+        email: "sanjay@company.com"
+    },
+    {
+        id: "EMP102",
+        name: "John",
+        department: "Development",
+        designation: "Software Engineer",
+        email: "john@company.com"
+    },
+    {
+        id: "EMP103",
+        name: "David",
+        department: "HR",
+        designation: "HR Executive",
+        email: "david@company.com"
+    },
+    {
+        id: "EMP104",
+        name: "Anita",
+        department: "Finance",
+        designation: "Accountant",
+        email: "anita@company.com"
+    },
+    {
+        id: "EMP105",
+        name: "Miraj",
+        department: "Support",
+        designation: "Technical Support Engineer",
+        email: "miraj@company.com"
+    },
+    {
+        id: "EMP106",
+        name: "Akash",
+        department: "Development",
+        designation: "Frontend Developer",
+        email: "akash@company.com"
+    },
+    {
+        id: "EMP107",
+        name: "Dev",
+        department: "DevOps",
+        designation: "DevOps Engineer",
+        email: "dev@company.com"
+    }
+];
